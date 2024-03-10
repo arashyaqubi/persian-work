da528124d84f0706a290c42626d1119045c1c015
arash <arashyaqubi71@gmail.com>
2024-03-10 16:28:45 +0330

    java sakhtam

---
+++ arash.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner arash = new Scanner(System.in);
+        int x = arash.nextInt();
+                System.out.println("please enter =>");
+        for(i=0;i<10;i++;){
+            System.out.println("\n"+i"*"+x+"="+x*i);
+        }
+    }
+}
